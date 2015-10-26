6f2714e4f0d17572e69669d607989993bcb6ac36
Mike Zornek <mike@clickablebliss.com>
2015-10-26 17:16:26 -0400

    Finished RW tutorial aspect of project.

--- PhillyCocoaHeadsTV/AppDelegate.swift
+++ PhillyCocoaHeadsTV/AppDelegate.swift
@@ -1,46 +1,28 @@
-//
-//  AppDelegate.swift
-//  PhillyCocoaHeadsTV
-//
-//  Created by Michael Zornek on 10/26/15.
-//  Copyright © 2015 Clickable Bliss. All rights reserved.
-//
-
 import UIKit
+import TVMLKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
+class AppDelegate: UIResponder, UIApplicationDelegate, TVApplicationControllerDelegate {
+    
     var window: UIWindow?
-
-
+    var appController: TVApplicationController?
+    static let TVBaseURL = "http://localhost:9001/"
+    static let TVBootURL = "\(AppDelegate.TVBaseURL)js/application.js"
+    
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        
+        let appControllerContext = TVApplicationControllerContext()
+        
+        guard let javaScriptURL = NSURL(string: AppDelegate.TVBootURL) else {
+            fatalError("unable to create NSURL")
+        }
+        appControllerContext.javaScriptApplicationURL = javaScriptURL
+        appControllerContext.launchOptions["BASEURL"] = AppDelegate.TVBaseURL
+        
+        appController = TVApplicationController(context: appControllerContext, window: window, delegate: self)
         return true
     }
 
-    func applicationWillResignActive(application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-    }
-
-    func applicationDidEnterBackground(application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(application: UIApplication) {
-        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
-
 }
-
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # PhillyCocoaHeadsTV
 An AppleTV app that lets you watch Philly CocoaHead presentations.
+
+# Running the server-content locally
+
+Since this is a TVML-style app it expect to get content from a web server. During development you'll want to run a simple web server locally and for that we recommend node. 
+
+Once you have node and npm install (we recommend using homebrew for that), install the web server with:
+
+`npm install http-server -g`
+
+Then within the `server-content` directory run the server on port 9001
+
+`http-server -p 9001`
+
+If you don't want to install node you can use OS X's default python to run a web server (but in my experience it can be a little buggy).
+
+`python -m SimpleHTTPServer 9001` 
