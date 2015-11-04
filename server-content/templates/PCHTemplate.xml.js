9aa3722f1d1f16c6d97edd54a8dfbca0efddbc39
Mike Zornek <mike@clickablebliss.com>
2015-11-03 22:38:14 -0500

    Adding auto-generated index file from go script.

--- server-content/templates/PCHTemplate.xml.js
+++ server-content/templates/PCHTemplate.xml.js
@@ -1,56 +1,65 @@
+
+// Before uploading: Replace the escaped less than symbol with a less than symbol 
+// FIXME: This sucks ^^
 var Template = function() { return `<?xml version="1.0" encoding="UTF-8" ?>
   <document>
-    <catalogTemplate> 
-      <banner> 
-        <title>RWDevConHighlights</title>
+    <catalogTemplate>
+      <banner>
+        <title>Philly CocoaHeads</title>
       </banner>
-      <list> 
-        <section> 
-	  <listItemLockup> 
-	    <title>Inspiration Videos</title>
-	    <decorationLabel>13</decorationLabel>
-	    <relatedContent> 
+      <list>
+        <section>
+	  <listItemLockup>
+	    <title>Recent Videos</title>
+	    <decorationLabel>11</decorationLabel>
+	    <relatedContent>
 	      <grid>
-	        <section> 
-		  <lockup videoURL="http://www.rwdevcon.com/videos/Ray-Wenderlich-Teamwork.mp4">
-		    <img src="${this.BASEURL}images/ray.png" width="500" height="308" />
-		  </lockup>
-		  <lockup videoURL="http://www.rwdevcon.com/videos/Ryan-Nystrom-Contributing.mp4">
-		    <img src="${this.BASEURL}images/ryan.png" width="500" height="308" />
-		  </lockup>									
-	          <lockup videoURL="http://www.rwdevcon.com/videos/Matthijs-Hollemans-Math-Isnt-Scary.mp4">
-		    <img src="${this.BASEURL}images/matthijs.png" width="500" height="308" />
-		  </lockup>									
-		  <lockup videoURL="http://www.rwdevcon.com/videos/Vicki-Wenderlich-Identity.mp4">
-		    <img src="${this.BASEURL}images/vicki.png" width="500" height="308" />
-		  </lockup>									
-	          <lockup videoURL="http://www.rwdevcon.com/videos/Alexis-Gallagher-Identity.mp4">
-		    <img src="${this.BASEURL}images/alexis.png" width="500" height="308" />
-	          </lockup>									
-		  <lockup videoURL="http://www.rwdevcon.com/videos/Marin-Todorov-RW-Folklore.mp4">
-		    <img src="${this.BASEURL}images/marin.png" width="500" height="308" />
-		  </lockup>									
-		  <lockup videoURL="http://www.rwdevcon.com/videos/Chris-Wagner-Craftsmanship.mp4">
-		    <img src="${this.BASEURL}images/chris.png" width="500" height="308" />
-	          </lockup>									
-		  <lockup videoURL="http://www.rwdevcon.com/videos/Cesare-Rocchi-Cognition.mp4">
-		    <img src="${this.BASEURL}images/cesare.png" width="500" height="308" />
-		  </lockup>									
-		  <lockup videoURL="http://www.rwdevcon.com/videos/Ellen-Shapiro-Starting-Over.mp4">
-		    <img src="${this.BASEURL}images/ellen.png" width="500" height="308" />
-		  </lockup>									
-		  <lockup videoURL="http://www.rwdevcon.com/videos/Jake-Gundersen-Opportunity.mp4">
-		    <img src="${this.BASEURL}images/jake.png" width="500" height="308" />
-		  </lockup>									
-		  <lockup videoURL="http://www.rwdevcon.com/videos/Kim-Pedersen-Finishing.mp4">
-		    <img src="${this.BASEURL}images/kim.png" width="500" height="308" />
-		  </lockup>									
-	          <lockup videoURL="http://www.rwdevcon.com/videos/Tammy-Coron-Possible.mp4">
-		    <img src="${this.BASEURL}images/tammy.png" width="500" height="308" />
-		  </lockup>									
-		  <lockup videoURL="http://www.rwdevcon.com/videos/Saul-Mora-NSBrief.mp4">
-		    <img src="${this.BASEURL}images/saul.png" width="500" height="308" />
-		  </lockup>		
+	        <section>
+            
+            <lockup videoURL="http://player.vimeo.com/external/140379332.hd.mp4?s=9722c1c6fada2684e86a48f4a0efdcc7389c19f9&amp;profile_id=119&amp;oauth2_token_id=384186094">
+              <img src="https://i.vimeocdn.com/video/536793873_640x360.jpg" width="500" height="282" />
+            </lockup>
+            
+            <lockup videoURL="http://player.vimeo.com/external/140377195.hd.mp4?s=f40f01a46df2e083b8f7e0f364a33f373b7e1ced&amp;profile_id=119&amp;oauth2_token_id=384186094">
+              <img src="https://i.vimeocdn.com/video/536793969_640x360.jpg" width="500" height="282" />
+            </lockup>
+            
+            <lockup videoURL="http://player.vimeo.com/external/137694857.sd.mp4?s=a9ef969c3bcc06825c404432f0d67e18d6f1991b&amp;profile_id=112&amp;oauth2_token_id=384186094">
+              <img src="https://i.vimeocdn.com/video/542448807_100x75.jpg" width="500" height="282" />
+            </lockup>
+            
+            <lockup videoURL="http://player.vimeo.com/external/137693009.hd.mp4?s=08039d6e6f2e0f070005c465431707d411f4dffb&amp;profile_id=119&amp;oauth2_token_id=384186094">
+              <img src="https://i.vimeocdn.com/video/532718226_640x360.jpg" width="500" height="282" />
+            </lockup>
+            
+            <lockup videoURL="http://player.vimeo.com/external/128605939.hd.mp4?s=9d904148569096bc3f6baa0d70e0c01c69706bdd&amp;profile_id=119&amp;oauth2_token_id=384186094">
+              <img src="https://i.vimeocdn.com/video/519812133_640x360.jpg" width="500" height="282" />
+            </lockup>
+            
+            <lockup videoURL="http://player.vimeo.com/external/124773837.hd.mp4?s=f2e0505f8e09ee50d1494f69fcfb729f9f974772&amp;profile_id=119&amp;oauth2_token_id=384186094">
+              <img src="https://i.vimeocdn.com/video/514628591_640x360.jpg" width="500" height="282" />
+            </lockup>
+            
+            <lockup videoURL="http://player.vimeo.com/external/124773343.hd.mp4?s=88e201bcbc798e06986abc1689dc636ba1d302d9&amp;profile_id=119&amp;oauth2_token_id=384186094">
+              <img src="https://i.vimeocdn.com/video/514628525_640x360.jpg" width="500" height="282" />
+            </lockup>
+            
+            <lockup videoURL="http://player.vimeo.com/external/122706243.hd.mp4?s=e41dcd4ac7777ef5c06fbbf4f6bdbb420fe53e40&amp;profile_id=119&amp;oauth2_token_id=384186094">
+              <img src="https://i.vimeocdn.com/video/511867104_640x360.jpg" width="500" height="282" />
+            </lockup>
+            
+            <lockup videoURL="http://player.vimeo.com/external/119634719.sd.mp4?s=dcf6984cd97335d6d7f8c439b534cbbb54d00b92&amp;profile_id=112&amp;oauth2_token_id=384186094">
+              <img src="https://i.vimeocdn.com/video/507137406_640x360.jpg" width="500" height="282" />
+            </lockup>
+            
+            <lockup videoURL="http://player.vimeo.com/external/119606531.hd.mp4?s=1c6c4d265da092ed652af7c0cf54b65cc9bec46f&amp;profile_id=113&amp;oauth2_token_id=384186094">
+              <img src="https://i.vimeocdn.com/video/507067373_640x360.jpg" width="500" height="282" />
+            </lockup>
+            
+            <lockup videoURL="http://player.vimeo.com/external/119604467.hd.mp4?s=4ad9f1c3f2440b5812f62e76bc80e72f84e0a67e&amp;profile_id=113&amp;oauth2_token_id=384186094">
+              <img src="https://i.vimeocdn.com/video/507064603_640x360.jpg" width="500" height="282" />
+            </lockup>
+            
 		</section>
 	      </grid>
 	    </relatedContent>
